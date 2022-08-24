d531f25c983f7f624011c3d7a77d45f2895f980d
BryantDev_ <Brayanmc.contacto@gmail.com>
2022-08-24 01:04:56 -0400

    👽 Feature: Added series page (work in progress)

--- src/views/series/index.jsx
+++ src/views/series/index.jsx
@@ -1,12 +1,38 @@
 import Footer from "components/footer";
 import Navbar from "components/navbar";
+import routes from "routes/api";
+import Section from "./components/section";
 
 export
 function Series() {
     return (
         <>
             <Navbar />
-            <div>HELLO SERIES</div>
+            <main className="series-page">
+                <Section
+                    sectionId="popular"
+                    url={routes.series.getPopular}
+                    sectionTitle="Most Popular Movies"
+                />
+
+                <Section
+                    sectionId="upcoming"
+                    url={routes.series.getUpcoming}
+                    sectionTitle="Upcoming Movies"
+                />
+
+                <Section
+                    sectionId="now-playing"
+                    url={routes.series.getNowPlaying}
+                    sectionTitle="Now Playing Movies"
+                />
+
+                <Section
+                    sectionId="top-rated"
+                    url={routes.series.getTopRated}
+                    sectionTitle="Top Rated Movies"
+                />
+            </main>
             <Footer />
         </>
     )
