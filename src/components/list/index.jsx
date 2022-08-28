7de84c27e1e8706177bdd10503e13083d5a48c26
BryantDev_ <Brayanmc.contacto@gmail.com>
2022-08-27 23:24:35 -0400

    🧐 Feature: Removed intersection observer from the list

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -1,50 +1,42 @@
-import { InView } from 'react-intersection-observer';
 import TitleWithLink from './TitleWithLink';
 import Loading from 'components/loading';
 
 import './list.scss';
-import { useState } from 'react';
 
 function List({ items, listTitle, urlToBeRedirected, isLoading, error, refetch, children, listId }) {
-    const [isInView, setIsInView] = useState(true);
     const theresTitle = listTitle;
     const redirect = urlToBeRedirected;
 
-    function handleView(isElementInView, entry) {
-        setIsInView(isElementInView);
-    }
-
     return (
-        <InView onChange={handleView}>
-            {isLoading || !isInView && <Loading />}
-            <div className="limited-list-container" id={listId}>
-                {
-                    theresTitle &&
-                    <div className="title-container">
-                        {
-                        redirect 
+        <div className="limited-list-container" id={listId}>
+            {
+                theresTitle &&
+                <div className="title-container">
+                    {
+                        redirect
                             ?
-                            
-                            <TitleWithLink 
-                             urlToBeRedirected={urlToBeRedirected}
-                             listTitle={listTitle}
+
+                            <TitleWithLink
+                                urlToBeRedirected={urlToBeRedirected}
+                                listTitle={listTitle}
                             />
 
-                            : 
+                            :
                             <h3>{listTitle}</h3>
-                        }
-                    </div>
-                }
-            
-            {
-                isInView &&
+                    }
+                </div>
+            }
+
+
+            <div>
+                {isLoading && <Loading />}
                 <ul className="limited-list">
                     {items}
                     {children}
                 </ul>
-            }
+            </div>
+
         </div>
-        </InView>
     )
 }
 
