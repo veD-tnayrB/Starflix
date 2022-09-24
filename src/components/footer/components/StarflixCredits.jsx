fb1df4e24d9118338a5f40eab0cdf7fa1c30e2b6
BryantDev_ <Brayanmc.contacto@gmail.com>
2022-09-24 12:38:26 -0400

    👾 Feature: Has been improved the footer

--- src/components/footer/components/StarflixCredits.jsx
+++ src/components/footer/components/StarflixCredits.jsx
@@ -3,12 +3,18 @@ import Logo from "components/logo"
 export default
 function StarflixCredits() {
     return (
-        <div className="starflix-credits">
-            <Logo />
-            <p className="credits">
-                Starflix is a web page that allows you to see advanced information about
-                your favorite movies, series, and people.
-            </p>
-        </div>
+        <a 
+            target="_blank"
+            href="https://github.com/veD-tnayrB/Starflix"
+            rel="noreferrer"
+        >
+            <div className="starflix-credits">
+                <Logo />
+                <p className="credits">
+                    Starflix is a web page that allows you to see advanced information about
+                    your favorite movies, series, and people.
+                </p>
+            </div>
+        </a>
     )
 }
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,7 @@
 import ApiCredits from "./components/ApiCedits";
 import Card from "./components/Card";
 import StarflixCredits from "./components/StarflixCredits";
+import CreatorCredits from "./components/CreatorCredits";
 import pages from './pages';
 
 import './footer.scss';
@@ -10,25 +11,29 @@ export default
 
     return (
         <footer>
-            <article className="credits-zone">
-                <StarflixCredits />
-                <ApiCredits />
-            </article>
+            <div className="first-row">
+                <article className="credits-zone">
+                    <StarflixCredits />
+                    <ApiCredits />
+                </article>
 
-            <div className="pages-section">
-                <Card
-                    info={pages.home}
-                />
-                <Card
-                    info={pages.movies}
-                />
-                <Card
-                    info={pages.series}
-                />
-                <Card
-                    info={pages.people}
-                />
+                <div className="pages-section">
+                    <Card
+                        info={pages.home}
+                    />
+                    <Card
+                        info={pages.movies}
+                    />
+                    <Card
+                        info={pages.series}
+                    />
+                    <Card
+                        info={pages.people}
+                    />
+                </div>
             </div>
+
+            <CreatorCredits />
         </footer>
     )
 }
