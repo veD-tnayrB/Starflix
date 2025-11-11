dd88688e045619353a26782b809eda7dd5c2b20b
Bryant Caballero <brayanmc.contacto@gmail.com>
2025-11-10 21:57:19 -0400

    fix: Solve hero component

--- src/views/home/components/hero/index.jsx
+++ src/views/home/components/hero/index.jsx
@@ -1,56 +1,55 @@
-import { useState, useEffect, memo } from 'react';
-import { getPopular } from 'services/movies';
-import { ORIGINAL_IMAGE_BASE_URL } from 'services/config';
 import AgeIndicator from 'components/age-indicator';
+import { memo, useEffect, useState } from 'react';
+import { ORIGINAL_IMAGE_BASE_URL } from 'services/config';
+import { getPopular } from 'services/movies';
 
-import './hero.scss';
 import { Link } from 'react-router-dom';
+import './hero.scss';
 
 const DEFAULT_VALUE = {
-    backdrop_path: '',
-    release_date: '',
-    adult: false
-}
+	backdrop_path: '',
+	release_date: '',
+	adult: false,
+};
 
 function Hero() {
-    const [popularMovie, setPopularMovie] = useState(DEFAULT_VALUE);
-    const backdropURL = `${ORIGINAL_IMAGE_BASE_URL}${popularMovie?.backdrop_path}`;
-    const releaseYear = popularMovie?.release_date.split('-')[0];
+	const [popularMovie, setPopularMovie] = useState(DEFAULT_VALUE);
+	const backdropURL = `${ORIGINAL_IMAGE_BASE_URL}${popularMovie?.backdrop_path}`;
+	const releaseYear = popularMovie?.release_date.split('-')[0];
 
-    useEffect(() => {
-        getPopular()
-            .then(response => {
-                const firstMovie = response.results[0];
-                setPopularMovie(firstMovie);
-            })
-    }, [])
+	useEffect(() => {
+		getPopular(null, 1).then(response => {
+			const firstMovie = response.results[0];
+			setPopularMovie(firstMovie);
+		});
+	}, []);
 
-    console.log(1, popularMovie)
-    return (
-        <>
-            <div className="movie-info-container">
-                <Link
-                    className="movie-link"
-                    to={`/movies/movie/${popularMovie.id}`}
-                >
-                    <h2 className="title">{popularMovie?.title}</h2>
-                </Link>
-                <div className="details-section">
-                    <span>{releaseYear}</span>
-                    <span>{popularMovie?.original_language}</span>
-                    {popularMovie?.overview && <AgeIndicator adult={popularMovie?.adult} />}
-                </div>
-                <p className="overview">{popularMovie?.overview}</p>
-            </div>
+	return (
+		<>
+			<div className="movie-info-container">
+				<Link
+					className="movie-link"
+					to={`/movies/movie/${popularMovie.id}`}>
+					<h2 className="title">{popularMovie?.title}</h2>
+				</Link>
+				<div className="details-section">
+					<span>{releaseYear}</span>
+					<span>{popularMovie?.original_language}</span>
+					{popularMovie?.overview && (
+						<AgeIndicator adult={popularMovie?.adult} />
+					)}
+				</div>
+				<p className="overview">{popularMovie?.overview}</p>
+			</div>
 
-            <img
-                className="backdrop"
-                alt={popularMovie?.title}
-                src={backdropURL}
-                loading="lazy"
-            />
-        </>
-    )
+			<img
+				className="backdrop"
+				alt={popularMovie?.title}
+				src={backdropURL}
+				loading="lazy"
+			/>
+		</>
+	);
 }
 
 export default memo(Hero);
